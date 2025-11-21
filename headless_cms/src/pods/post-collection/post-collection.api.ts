c9dc1a9fa7f081eaa57577a191b164a48ab5a863
qv1ko <vicgarmur947@gmail.com>
2025-11-21 20:34:23 +0100

    feat: added list of publications

---
+++ headless_cms/src/pods/post-collection/post-collection.api.ts
@@ -0,0 +1,8 @@
+import client from "#lib/client.ts";
+import type { Post } from "./post-collection.model";
+
+export const getAllPost = async () => await client.getContentList<Post>({
+    contentType: 'Post',
+    sort: { 'fields.date': 'desc' },
+    pagination: { take: 6 }
+});
