892b82e4032ab260bc19a9c724ff560b46562c48
qv1ko <vicgarmur947@gmail.com>
2025-11-22 23:32:14 +0100

    feat: added posts with dynamic paths

--- headless_cms/src/pods/post-collection/post-collection.api.ts
+++ headless_cms/src/pods/post-collection/post-collection.api.ts
@@ -1,7 +1,7 @@
 import client from "#lib/client.ts";
 import type { Post } from "./post-collection.model";
 
-export const getAllPost = async () => await client.getContentList<Post>({
+export const getAllPosts = async () => await client.getContentList<Post>({
     contentType: 'Post',
     sort: { 'fields.date': 'desc' },
     pagination: { take: 6 }
