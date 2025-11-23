784941a197b111bfda609915de890a93bed872ec
qv1ko <vicgarmur947@gmail.com>
2025-11-23 13:08:45 +0100

    feat: integrating React

--- headless_cms/astro.config.mjs
+++ headless_cms/astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
+import react from '@astrojs/react';
 import { defineConfig, envField } from "astro/config";
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [react()],
   vite: {
     plugins: /** @type {any} */ ([tailwindcss()]),
   },
